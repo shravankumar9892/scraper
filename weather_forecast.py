fcd82fd7089792e24352f2bb2cfb8ce8c46d1cc2
Shravan Shetty <shravankumarshetty9892@gmail.com>
2018-12-09 21:15:36 +0530

    dataframe created
    Weather information correctly scrapped and put into a dataframe.

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 url = 'https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168#.XAz1Ty4zbdg'
@@ -8,17 +9,18 @@ page = requests.get(url)
 # BeautifulSoup Class
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# Filtering data
 seven_day = soup.find('div', id = 'seven-day-forecast')
 forecast_items = seven_day.find_all(class_ = 'tombstone-container')
 
-def day(box): # For the present day, it show todayand not name of the day.
-	day = box.find('p', class_ = "period-name").get_text()	
-	return day
+# Getting indv info
+temperature = [x.get_text() for x in seven_day.select(".tombstone-container .temp")]
 
-	
-# Weather data of next week.
-num = 0
-for element in forecast_items:
-	print("(%d) "%num + str(element) + "\n")
-	num += 1
+weather = [x.get_text() for x in seven_day.select(".tombstone-container .short-desc")]
 
+day = [x.get_text() for x in seven_day.select(".tombstone-container .period-name")]
+
+# Creating a dataframe
+week = pd.DataFrame({'Temperature': temperature,
+		     'Weather': weather}, index = day)
+print(week)
